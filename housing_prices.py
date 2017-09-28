b42b81ec3ff98a436bfa1a817a31e73c1ee5c58e
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-09-27 21:39:29 -0700

    using plots to find relevant features;

--- housing_prices.py
+++ housing_prices.py
@@ -1,6 +1,8 @@
 # import data modules
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # import sklearn modules
 from sklearn.metrics import r2_score, mean_squared_error, make_scorer
@@ -20,17 +22,31 @@ def main():
     testing_set = pd.read_csv('data/test.csv')
     # print training_set.head()
     # print testing_set.head()
+    print training_set.columns
+    print training_set['SalePrice'].describe()
+
+    # scatter plot grlivarea/saleprice
+    # data = pd.concat([training_set['SalePrice'], training_set['OverallQual']], axis=1)
+    # data.plot.scatter(x='OverallQual', y='SalePrice', ylim=(0,800000));
+    
+
+    # correlation matrix
+    corrmat = training_set.corr()
+    f, ax = plt.subplots(figsize=(12, 9))
+    sns.heatmap(corrmat, vmax=.8, square=True);
+    plt.show()
 
     # split training set to features and labels
     training_set_labels = training_set['SalePrice']
     training_set_features = training_set.drop('SalePrice', axis=1)
     
     # TODO: clean up and preprocess data
-    # 1. drop irrelevant/invalid features
+    # 1. drop irrelevant/invalid features 
+        # remove features with null values
     # 2. label encoding on features requiring transformation
 
     # test with a few relevant features only
-    relevant_features = ['OverallQual', 'OverallCond', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF']
+    relevant_features = ['OverallQual', 'OverallCond', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea']
     training_set_features = training_set_features[relevant_features]
 
     # shuffle and split training data
@@ -47,8 +63,7 @@ def main():
     # predict on test sets
     pred = regressor_model.predict(X_test)
     # perf_metric_rmse(y_test, pred)
-    print "R2 score from model prediction:"
-    perf_metric_r2(y_test, pred)
+    print "R2 score from model prediction: {0:.3f}".format(perf_metric_r2(y_test, pred))
 
 '''
 Performs grid search cross validation over max depth parameter to get optimal decision tree regressor model
@@ -82,7 +97,7 @@ Calculate and return r2 score.
 def perf_metric_r2(y_true, y_predict):
     # calculate r2 score and print
     r2 = r2_score(y_true, y_predict)
-    print "r2 score: {0:.3f}".format(r2)
+    # print "r2 score: {0:.3f}".format(r2)
     return r2
 
 '''
@@ -91,7 +106,7 @@ Calculate and return RMSE.
 def perf_metric_rmse(y_true, y_predict):
     # calculate RMSE 
     rmse = mean_squared_error(y_true, y_predict)
-    print "RMSE score: {0:.3f}".format(rmse)
+    # print "RMSE score: {0:.3f}".format(rmse)
     return rmse
     
 '''
